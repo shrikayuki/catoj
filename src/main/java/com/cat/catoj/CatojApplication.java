7311f435f1d4b0c91dafbc91ebf5d96d32c7bd9c
shinku <1322246106@qq.com>
2026-06-15 15:24:39 +0800

    Initial commit

---
+++ src/main/java/com/cat/catoj/CatojApplication.java
@@ -0,0 +1,13 @@
+package com.cat.catoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatojApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cat/catoj/CatojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cat.catoj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
